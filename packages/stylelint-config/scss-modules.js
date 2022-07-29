f87f0c8ffccf922159abe262f5e7882e31cbd595
Alex <akh.default@gmail.com>
2022-07-29 19:41:28 +0300

    A fix

--- packages/stylelint-config/scss-modules.js
+++ packages/stylelint-config/scss-modules.js
@@ -1,10 +1,12 @@
 /** @type {import('stylelint').Config} */
 const config = {
-  extends: ['./css-modules', './scss'],
+  extends: ['./scss', './css-modules'],
   rules: {
     // camelCase
     'scss/at-mixin-pattern': '^[a-z][a-zA-Z0-9]+$',
     'scss/dollar-variable-pattern': '^[a-z][a-zA-Z0-9]+$',
+    // repeat what's already in ./scss
+    'at-rule-no-unknown': null,
   },
 };
 
