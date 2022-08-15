b35a96bb83db142b817592032dd0ae527f2a0a3f
Alex <akh.default@gmail.com>
2022-08-15 17:46:26 +0300

    Patch eslint to allow plugins as deps in shareable config

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,3 +1,6 @@
+// Fix eslint shareable config (https://github.com/eslint/eslint/issues/3458)
+require('@rushstack/eslint-patch/modern-module-resolution');
+
 /** @type {import('eslint').Linter.Config} */
 const config = {
   extends: ['standard', 'prettier'],
