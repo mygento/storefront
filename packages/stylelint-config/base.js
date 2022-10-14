c1b8329fb2578e1262b4f3d5d29f87ba22820daf
Alex Xodov <a.hodov@mygento.ru>
2022-10-14 20:06:09 +0400

    Handle formatting styles via prettier

--- packages/stylelint-config/base.js
+++ packages/stylelint-config/base.js
@@ -1,25 +1,11 @@
 /** @type {import('stylelint').Config} */
 const config = {
   extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
-  plugins: ['stylelint-order'],
+  plugins: ['stylelint-order', 'stylelint-prettier'],
   rules: {
     // alphabetic order
     'order/order': ['custom-properties', 'declarations'],
     'order/properties-alphabetical-order': true,
-
-    // formatting
-    'declaration-colon-newline-after': 'always-multi-line',
-    'rule-empty-line-before': [
-      'always',
-      { except: ['first-nested', 'after-single-line-comment'] },
-    ],
-    'at-rule-empty-line-before': [
-      'always',
-      { except: ['first-nested', 'blockless-after-same-name-blockless'] },
-    ],
-    'declaration-empty-line-before': 'never',
-    'declaration-no-important': true,
-
     // mobile first
     'media-feature-name-disallowed-list': [
       'max-width',
@@ -27,9 +13,9 @@ const config = {
         message: 'Use min-width for a mobile-first approach',
       },
     ],
-
     // works with a systemic design - disable if impractical in a specific project
     'color-named': 'always-where-possible',
+    'prettier/prettier': true,
   },
 };
 
