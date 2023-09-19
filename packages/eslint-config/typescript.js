65eedaa01e6b16dd6ba346acc466822717c4108b
Alex Xodov <a.hodov@mygento.ru>
2023-09-19 15:31:34 +0400

    Update deps

--- packages/eslint-config/typescript.js
+++ packages/eslint-config/typescript.js
@@ -1,5 +1,7 @@
 module.exports = {
   extends: ['plugin:@typescript-eslint/recommended'],
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  parser: '@typescript-eslint/parser',
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
@@ -7,7 +9,7 @@ module.exports = {
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/consistent-type-definitions': ['warn', 'type'],
     '@typescript-eslint/no-inferrable-types': 'warn',
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
   },
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint/eslint-plugin'],
 };
